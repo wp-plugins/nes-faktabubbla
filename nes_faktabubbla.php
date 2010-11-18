001c9a7c87e4fda025e539e52ff721dfd6e7bef1
Nationalencyklopedin <Nationalencyklopedin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-18 10:58:18 +0000

    git-svn-id: https://plugins.svn.wordpress.org/nes-faktabubbla/trunk@312869 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nes_faktabubbla.php
+++ nes_faktabubbla.php
@@ -6,11 +6,8 @@ Description: Aktiverar NE:s faktabubbla på din sida. Faktabubblan dyker upp nä
 Version: 1.0
 Author: Nationalencyklopedin
 Author URI: http://www.ne.se
-
 */
 
-
-// add tags as keywords
 function nebubble_add_bubble(){
    
         echo '<script src="http://media.ne.se/lib/js/external/jquery/jquery-current.js" type="text/javascript"></script>
@@ -18,10 +15,6 @@ function nebubble_add_bubble(){
 <script type="text/javascript">NEBubbla.showSearch=true;</script>'; // add script tag to <head>
         
 }
-
-
-
-
 
 
 add_action('wp_head','nebubble_add_bubble'); // add ne bubble insertion code to wp_head function
